c75db3d07baaa7de623e908b23192076ab014e2c
Andrey Baskakov <andreybaskakov@mail.ru>
2022-09-13 15:25:13 +0300

    - xCode14 compilation fixed

--- Source/Charts/Animation/ChartAnimationEasing.swift
+++ Source/Charts/Animation/ChartAnimationEasing.swift
@@ -341,7 +341,8 @@ internal struct EasingFunctions
         let s: TimeInterval = 1.70158
         var position: TimeInterval = elapsed / duration
         position -= 1.0
-        return Double( position * position * ((s + 1.0) * position + s) + 1.0 )
+        let position2 = position * position
+        return Double( position2 * ((s + 1.0) * position + s) + 1.0 )
     }
     
     internal static let EaseInOutBack = { (elapsed: TimeInterval, duration: TimeInterval) -> Double in
